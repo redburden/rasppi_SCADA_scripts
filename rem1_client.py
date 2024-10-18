5cac3da4aa9b6d9873406906a05c0372790992e3
redburden <90938076+redburden@users.noreply.github.com>
2024-10-18 21:44:52 +0100

    try using queue for motor state

--- rem1_client.py
+++ rem1_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """ Read 10 coils and print result on stdout. """
-from multiprocessing import Process, Value, Array
+from multiprocessing import Process, Queue
 import time
 import RPi.GPIO as GPIO
 
@@ -18,86 +18,85 @@ from pyModbusTCP.client import ModbusClient
 c = ModbusClient(host='100.107.143.47', port=502, auto_open=True)
 m1_speed = 0
 m1_hand_speed = 0
-pulse_time = 0
+m1_run_cmd = False
+m1_q = Queue()
 
-
-def run_M1(pulse_time):
-    while True:
+def run_M1(q):
+    while m1_run_cmd:
+        # if there is something in the queue, pull it
+        if not q.empty():
+            m1_run_data = q.get()
+            m1_run_cmd = m1_run_data[0]
         GPIO.output(26,GPIO.HIGH)
-        time.sleep(pulse_time)
+        time.sleep(m1_run_data[1])
         GPIO.output(26,GPIO.LOW)
+    GPIO.output(26,GPIO.LOW)
+    return 0
 
-def m1_proc():
-    print("Motor is being told to run.")
-    if p.is_alive() == True:
-        p.join()
-        p.terminate()
-        p.start()
-    else:
-        p.start()
-
-# main read loop
-while True:
-    # read 10 bits (= coils) at address 0, store result in coils list
-    coils_l = c.read_coils(0, 15)
-    c.write_single_register(3, m1_speed)
-    m1_remote_speed = c.read_holding_registers(0, 1)[0]
-    p = Process(target=run_M1, args=(pulse_time,))
-
-    try:
-        # Read an analog value written to ana_val.txt
-        with open('ana_val.txt', 'r') as f:
-            m1_hand_speed = int(f.read())
-    except:
-        m1_hand_speed = 0
-    else:
-        f.close()
-
-    # if success display registers
-    if coils_l:
-        if coils_l[0] == True:
-            GPIO.output(3,GPIO.HIGH)
+if __name__ == '__main__':    
+    # main read loop
+    p = Process(target=run_M1, args=(m1_q,))
+    while True:
+        # read 10 bits (= coils) at address 0, store result in coils list
+        coils_l = c.read_coils(0, 15)
+        c.write_single_register(3, m1_speed)
+        m1_remote_speed = c.read_holding_registers(0, 1)[0]
+        try:
+            # Read an analog value written to ana_val.txt
+            with open('ana_val.txt', 'r') as f:
+                m1_hand_speed = int(f.read())
+        except:
+            m1_hand_speed = 0
         else:
-            GPIO.output(3,GPIO.LOW)
-        if coils_l[1] == True:
-            GPIO.output(17,GPIO.HIGH)
-        else:
-            GPIO.output(17,GPIO.LOW)
-        if coils_l[2] == True:
-            GPIO.output(27,GPIO.HIGH)
-        else:
-            GPIO.output(27,GPIO.LOW)
-        if coils_l[10] == True:
-            print(coils_l[10], coils_l[11], m1_speed, m1_remote_speed, m1_hand_speed)
-            if coils_l[11] == False:
-                # Test if m1_remote_speed is different from m1_speed by more than 5
-                if m1_remote_speed == 0:
-                    if p.is_alive() == True:
-                        p.join()
-                        p.terminate()
-                elif abs(m1_remote_speed - m1_speed) > 1:
-                    m1_speed = m1_remote_speed
-                    pulse_time = 0.004 + (100/m1_speed) * 0.001
-                    m1_proc()
+            f.close()
+        # if success display registers
+        if coils_l:
+            if coils_l[0] == True:
+                GPIO.output(3,GPIO.HIGH)
             else:
-                if m1_hand_speed == 0:
-                    if p.is_alive() == True:
-                        p.join()
-                        p.terminate()
-                elif abs(m1_hand_speed - m1_speed) > 1:
-                    m1_speed = m1_hand_speed
-                    0.004 + (100/m1_speed) * 0.001
-                    m1_proc()
+                GPIO.output(3,GPIO.LOW)
+            if coils_l[1] == True:
+                GPIO.output(17,GPIO.HIGH)
+            else:
+                GPIO.output(17,GPIO.LOW)
+            if coils_l[2] == True:
+                GPIO.output(27,GPIO.HIGH)
+            else:
+                GPIO.output(27,GPIO.LOW)
+            if coils_l[10] == True:
+                print(coils_l[10], coils_l[11], m1_speed, m1_remote_speed, m1_hand_speed)
+                if coils_l[11] == False:
+                    if m1_remote_speed == 0:
+                        GPIO.output(26,GPIO.LOW)
+                        while not m1_q.empty():
+                            m1_q.get()
+                        m1_q.put([False, 0])                           
+                    elif abs(m1_remote_speed - m1_speed) > 1:
+                        m1_speed = m1_remote_speed
+                        pulse_time = 0.004 + (100/m1_speed) * 0.001
+                        m1_q.put([True, pulse_time])
+                        if not p.is_alive():
+                            p.start()
+                            p.join()
+                else:
+                    if m1_hand_speed == 0:
+                        GPIO.output(26,GPIO.LOW)
+                        while not m1_q.empty():
+                            m1_q.get()
+                        m1_q.put([False, 0])   
+                    elif abs(m1_hand_speed - m1_speed) > 1:
+                        m1_speed = m1_hand_speed
+                        0.004 + (100/m1_speed) * 0.001
+                        m1_q.put([True, pulse_time])
+                        if not p.is_alive():
+                            p.start()
+                            p.join()
+            else:
+                print("Motor is being told to stop.")                
+                GPIO.output(26,GPIO.LOW)
+                while not m1_q.empty():
+                    m1_q.get()
+                m1_q.put([False, 0])
         else:
-            print("Motor is being told to stop.")
-            
-            GPIO.output(26,GPIO.LOW)
-            if p.is_alive() == True:
-                p.join()
-                p.kill()            
-            print(str(p.is_alive()))
-    else:
-        print('unable to read coils')
-
-    # sleep 2s before next polling
-    time.sleep(0.1)
+            print('unable to read coils')
+        time.sleep(0.1)
