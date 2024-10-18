f7acbc23cdcf1783f00a12865427527c57f1a800
redburden <90938076+redburden@users.noreply.github.com>
2024-10-18 18:02:48 +0100

    add index to read holding reg

--- rem1_client.py
+++ rem1_client.py
@@ -41,7 +41,7 @@ while True:
     # read 10 bits (= coils) at address 0, store result in coils list
     coils_l = c.read_coils(0, 15)
     c.write_single_register(3, m1_speed)
-    m1_remote_speed = c.read_holding_registers(0, 1)
+    m1_remote_speed = c.read_holding_registers(0, 1)[0]
     p = Process(target=run_M1, args=(pulse_time,))
 
     try:
